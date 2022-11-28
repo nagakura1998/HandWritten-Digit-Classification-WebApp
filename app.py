267858e203ebf6b8fd8eb2a41f25673de0e7a3d5
nagakura1998 <khiempro12c2@gmail.com>
2022-11-28 13:09:47 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import base64
+import json
+from io import BytesIO
+from PIL import Image
+import cnn
+
+app = Flask(__name__, template_folder='template')
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    b = base64.urlsafe_b64decode(request.form['operation'])
+    operation = BytesIO(b)
+    result = cnn.predict(operation)
+    print(result.item())
+    #flash("success")
+    return json.dumps({
+        #'operation': operation,
+        'solution': result.item()
+    })
+
+if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.run(debug=True, host='0.0.0.0', port=5000)
